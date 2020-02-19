77b79c97b23223369463f9979d49c02f011f5a76
Callum Sargeant <callum.sargeant@gmail.com>
2020-02-19 17:21:18 +0000

    teach discord bot to turn LaTeX eqn into image

--- bot.py
+++ bot.py
@@ -1,5 +1,6 @@
 import discord
 import os
+import latex
 
 client = discord.Client()
 
@@ -12,7 +13,10 @@ async def on_message(message):
     if message.author == client.user:
         return
 
-    if message.content.startswith('$hello'):
-        await message.channel.send('Hello!')
+    if message.content.startswith('$latex'):
+        input_expr = message.content.split(None, 1)[1]
+        png = latex.GeneratePNGFromExpression(input_expr)
+        if png is not None:
+            await message.channel.send(file=discord.File(png, 'image.png'))
 
 client.run(os.getenv('DISCORD_TOKEN'))
---
+++ latex/__init__.py
@@ -0,0 +1,42 @@
+import os
+import io
+import tempfile
+import subprocess
+
+def GeneratePNGFromExpression(latex_expression):
+    with tempfile.TemporaryDirectory() as tmpdir:
+        tex_file = os.path.join(tmpdir, 'expression.tex')
+        with open(tex_file, 'w') as output:
+            output.write('''\
+\\documentclass[varwidth,12pt]{standalone}
+\\usepackage{amsmath}
+\\usepackage{amsfonts}
+\\begin{document}
+\\[
+''')
+            output.write(latex_expression)
+            output.write('''\
+\\]
+\\end{document}
+''')
+        result = subprocess.run([
+                "latex", "-halt-on-error", "-interaction=nonstopmode", "expression.tex"
+            ], cwd=tmpdir)
+        if result.returncode != 0:
+            return None
+        result = subprocess.run([
+                "dvipng", 
+                "-D", "250", 
+                "-T", "tight", 
+                "-z", "9",
+                #"-bg", "Transparent", 
+                "-o", "expression.png", 
+                "expression.dvi"
+            ], cwd=tmpdir)
+        if result.returncode != 0:
+            return None
+        return io.BytesIO(open(os.path.join(tmpdir, "expression.png"), "rb").read())
+
+__all__ = [
+    'GeneratePNGFromExpression'
+]
