fbd312818304f54682e7f2a5405ad3f042dc93af
Callum Sargeant <callum.sargeant@gmail.com>
2020-02-19 13:54:22 +0000

    added proper parsing grammar

--- parser/__init__.py
+++ parser/__init__.py
@@ -1,7 +1,10 @@
 from lark import lark
 import os
 
-parser = lark.Lark(open(os.path.join(os.path.dirname(__file__), 'grammar.lark')))
+parser = lark.Lark(
+    open(os.path.join(os.path.dirname(__file__), 'grammar.lark')),
+    lexer = "standard")
+
 
 __all__ = [
     'parser'
--- parser/tests.py
+++ parser/tests.py
@@ -2,6 +2,36 @@ import unittest
 from . import parser
 
 class TestParser(unittest.TestCase):
-    def test_parser(self):
-        tree = parser.parse('Hello, World!')
-        print(tree)
+    def check_parses(self, string):
+        tree = parser.parse(string)
+        # print(tree.pretty())
+
+    def test_expressions(self):
+        self.check_parses('2')
+        self.check_parses('2.0')
+        self.check_parses('x')
+        self.check_parses("-x")
+        self.check_parses('sin(x)')
+        self.check_parses('pi')
+        self.check_parses('x^2')
+        self.check_parses('2a')
+        self.check_parses('2ax')
+        self.check_parses('2a^2')
+        self.check_parses('2a^2x')
+        self.check_parses('2sin(x)cos(y)')
+        self.check_parses('sinh(x)')
+        self.check_parses('((x))')
+        self.check_parses('(y(x))')
+        self.check_parses('2-3')
+        self.check_parses('a-b-c')
+        self.check_parses('a-(b-c)')
+        self.check_parses('2--3')
+        self.check_parses('x-y')
+        self.check_parses('x^(-y)')
+        self.check_parses('x^(5-y)')
+        self.check_parses('a/b/c')
+        self.check_parses('1*2*3')
+        self.check_parses('2+3/4')
+        self.check_parses('2/xy')
+        self.check_parses('2/x*y')
+        self.check_parses('ln(abs(x))')
