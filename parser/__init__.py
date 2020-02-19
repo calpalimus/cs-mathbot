9c6b95d38d8f733c429fc30580f4b6700d2882d4
Callum Sargeant <callum.sargeant@gmail.com>
2020-02-19 17:20:07 +0000

    add support for turning parse trees into strings
    then use this in unit tests

--- parser/__init__.py
+++ parser/__init__.py
@@ -1,11 +1,46 @@
-from lark import lark
+from lark import Lark, Transformer, v_args
 import os
 
-parser = lark.Lark(
+parser = Lark(
     open(os.path.join(os.path.dirname(__file__), 'grammar.lark')),
     lexer = "standard")
 
+@v_args(inline=True)
+class StringTransformer(Transformer):
+    def add(self, lhs, rhs):
+        return '(+ %s %s)' % (lhs, rhs)
+
+    def sub(self, lhs, rhs):
+        return '(- %s %s)' % (lhs, rhs)
+
+    def div(self, lhs, rhs):
+        return '(/ %s %s)' % (lhs, rhs)
+
+    def mul(self, *arguments):
+        return '(* ' + ' '.join(arguments) + ')'
+
+    def constant(self, CONSTANT):
+        return '(%s)' % (CONSTANT.value,)
+
+    def neg(self, arg):
+        return '(- %s)' % (arg,)
+
+    def exp(self, lhs, rhs):
+        return '(^ %s %s)' % (lhs, rhs)
+
+    def function(self, FUNCTION, argument):
+        return '(%s %s)' % (FUNCTION.value, argument)
+
+    def number(self, NUMBER):
+        return NUMBER.value
+
+    def variable(self, VARIABLE):
+        return VARIABLE.value
+
+def ParseTreeToString(parse_tree):
+    return StringTransformer().transform(parse_tree)
 
 __all__ = [
-    'parser'
+    'parser',
+    'ParseTreeToString'
 ]
